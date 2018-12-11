c6073c349c7c506cec910faad6cf2d6aaf72e00d
makeshwari <45779500+makeshwari@users.noreply.github.com>
2018-12-11 14:17:08 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,7 +1,7 @@
-x=('a' or 'e' or 'i' or 'o' or 'u')
-y=('b' or 'c' or 'd' or 'f' or 'g' or 'h' or 'j' or 'k'
+X=('a' or 'e' or 'i' or 'o' or 'u')
+Y=('b' or 'c' or 'd' or 'f' or 'g' or 'h' or 'j' or 'k'
     or 'l' or 'm' or 'n' or 'p' or 'q' or 'r' or 's' or 't' or 'v' or 'w' or 'x' or 'y' or 'z')
-if(x!=y):
- print("vowel")
+if(X!=Y):
+ print("Vowel")
 else:
- print("consonant")
+ print("Consonant")
