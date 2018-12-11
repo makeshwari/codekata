c4d8c6f28b585a874e9a82e6e7638fec84d70b02
makeshwari <45779500+makeshwari@users.noreply.github.com>
2018-12-11 14:14:24 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,5 +1,6 @@
 x=('a' or 'e' or 'i' or 'o' or 'u')
-y='f'
+y=('b' or 'c' or 'd' or 'f' or 'g' or 'h' or 'j' or 'k'
+    or 'l' or 'm' or 'n' or 'p' or 'q' or 'r' or 's' or 't' or 'v' or 'w' or 'x' or 'y' or 'z')
 if(x!=y):
  print("vowel")
 else:
