bce83f617fe188c06c039a27a126140ef5fe6bbd
makeshwari <45779500+makeshwari@users.noreply.github.com>
2018-12-23 08:24:37 +0530

    Update number.py

--- number.py
+++ number.py
@@ -1,22 +1,22 @@
 class listnode(object):
  def_init_(self,y):
-  self.val=y
+  self.value=y
   self.next=none
 class solution(object):
  def addTwoNumber(self,11,12):
   carry=0
   head=curr=listnode(0)
   while 11 or 12
-   val=carry
+   value=carry
    if 11:
-    val+=11.val
-    11=11.val
+    value+=11.value
+    11=11.value
    if 12:
-    val+=12.val
+    value+=12.value
     12=12.next
-   curr.next=listnode(val%10)
+   curr.next=listnode(value%10)
    curr+curr.next
-   carry=val/10
+   carry=value/10
   if carry>0:
    curr.next=listnode(carry)
   return head.next 
